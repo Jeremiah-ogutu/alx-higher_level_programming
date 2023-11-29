c65eb61b7515a3e7fde9173f882c86b73e1ed60d
Jeremiah-ogutu <jeremiahjuma040@gmail.com>
2023-11-29 15:43:51 +0300

    9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
-def pow(j, o):
-    return (j ** o)
+def print_last_digit(number):
+    if number < 0:
+        last_digit = number % -(10)
+        print(-(last_digit), end='')
+    else:
+        last_digit = number % 10
+        print(last_digit, end='')
+    return abs(last_digit)
